2752f71967ed51940f626394f22307c40d45ba63
Swati <agrswati2794@gmail.com>
2023-12-11 22:41:52 -0800

    add browse page components + implemt video title

---
+++ netflix-gpt-swati/src/components/MainContainer.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import VideoTitle from './VideoTitle'
+import VideoBackground from './VideoBackground'
+import { useSelector } from 'react-redux'
+
+const MainContainer = () => {
+
+    const movies = useSelector((store) => store.movies?.nowPlayingMovies);
+    if(!movies) return;// if(movies===null)
+    const mainMovie = movies[0];
+    console.log(mainMovie);
+
+    const { original_title, overview } = mainMovie;
+
+  return (
+    <div>
+        <VideoTitle title = {original_title} overview = {overview}/>
+        <VideoBackground/>
+    </div>
+  )
+}
+
+export default MainContainer
