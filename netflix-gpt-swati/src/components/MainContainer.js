518c898c627e2fddafcf4a856fc3ae083c97bb4c
Swati <agrswati2794@gmail.com>
2023-12-11 23:14:12 -0800

    make getMovieVideos API call and ember VideoBackground

--- netflix-gpt-swati/src/components/MainContainer.js
+++ netflix-gpt-swati/src/components/MainContainer.js
@@ -10,12 +10,12 @@ const MainContainer = () => {
     const mainMovie = movies[0];
     console.log(mainMovie);
 
-    const { original_title, overview } = mainMovie;
+    const { original_title, overview, id } = mainMovie;
 
   return (
     <div>
         <VideoTitle title = {original_title} overview = {overview}/>
-        <VideoBackground/>
+        <VideoBackground movieId = {id}/>
     </div>
   )
 }
